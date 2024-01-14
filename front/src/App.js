0b2b6f3250437babb6c60da697c997b85844d917
Chosamee <cytor@naver.com>
2024-01-15 00:39:18 +0900

    add: Add init setting

---
+++ front/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import JanusPage from "./pages/JanusPage";
+
+function App() {
+  return (
+    <div className="App">
+      <JanusPage />
+    </div>
+  );
+}
+
+export default App;
