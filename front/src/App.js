b96ad7fcdc46ff7fe528817390ce4ae7cb7f1c8b
bogeun <kimbo3212@gmail.com>
2024-01-16 17:49:46 +0900

    네이버 소셜 로그인 기능 구현중

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,41 @@
-import logo from "./logo.svg";
-import "./App.css";
-import JanusPage from "./pages/JanusPage";
+// import logo from "./logo.svg";
+// import "./App.css";
+// import JanusPage from "./pages/JanusPage";
 
-function App() {
-  return (
-    <div className="App">
-      <JanusPage />
-    </div>
-  );
-}
+// function App() {
+//   return (
+//     <div className="App">
+//       <JanusPage />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// App.js
+import React, { useState } from 'react';
+import LoginButton from './components/LoginButton';
+
+const App = () => {
+    const [user, setUser] = useState(null);
+
+    const handleLogin = (result) => {
+        setUser(result.user);
+        // 이후 처리 로직을 추가할 수 있습니다.
+    };
+
+    return (
+        <div>
+            {user ? (
+                <div>
+                    <p>Welcome, {user.name}!</p>
+                    <p>Email: {user.email}</p>
+                </div>
+            ) : (
+                <LoginButton onLogin={handleLogin} />
+            )}
+        </div>
+    );
+};
 
 export default App;
