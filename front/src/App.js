2b4cb5c4cba8e01760c90a2de13b84114844258b
Chosamee <cytor@naver.com>
2024-01-17 14:59:09 +0900

    feat: Add sample google login component

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Main from './pages/MainPage';
-import Signup from './pages/Signup';
-import Login from './pages/Login';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Main from "./pages/MainPage";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import GoogleLoginComp from "./components/GoogleLoginComp";
 
 function App() {
   return (
-      <BrowserRouter>
-
-        <Navbar/>
+    <BrowserRouter>
+      <Navbar />
 
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        
+        <Route path="/google_login" element={<GoogleLoginComp />} />
       </Routes>
-      </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
--- front/src/api/authService.js
+++ front/src/api/authService.js
@@ -1,16 +1,26 @@
 import axios from "axios";
-const naverLogin = async (username, password) => {
+
+// 아래와 같이 .env에 주소를 넣고 변경
+// const API_BASE_URL = process.env.REACT_APP_API_URL;
+// 개발환경에선 우선 localhost:8000 적용
+const API_BASE_URL = "http://localhost:8000";
+
+export const handleGoogleLogin = async (googleData) => {
+  const formData = new FormData();
+  formData.append("token", googleData.credential);
+
   try {
-    const response = await axios.post("/", {
-      username,
-      password,
-    });
-    console.log("로그인 성공:");
-    return response;
+    const response = await axios.post(
+      `${API_BASE_URL}/google_token`,
+      formData,
+      {
+        withCredentials: true,
+      }
+    );
+    console.log("Google Login Success:", response);
+    return response.data;
   } catch (error) {
-    console.log("에러 발생");
+    console.error("Error during Google Login:", error);
     throw error;
   }
 };
-
-export { naverLogin };
