7b6aa3e18e0d1547e0d10137dd27936989b9a8ca
Chosamee <cytor@naver.com>
2024-01-22 14:56:33 +0900

    feat: Add normal auth service -ing

--- back/app/models/user.py
+++ back/app/models/user.py
@@ -1,55 +1,41 @@
 # app/models/user.py
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, Enum
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, ForeignKey, Enum, Text
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from app.database import Base  # database.py에서 Base 클래스를 임포
-
-
-class UserType(Enum):
-    individual = "individual"
-    business = "business"
 
 
 class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
+    internal_id = Column(String, unique=True, index=True)  # 모든 유저에 대한 고유 식별자
     email = Column(String, index=True)
-    nickname = Column(String, unique=True, index=True)
+    name = Column(String)
 
-    password_hash = Column(String)
-    user_type = Column(String)
-    additional_info_submitted = Column(Boolean, default=False)
+    hashed_password = Column(String)  # 실제 저장되는 비밀번호는 해시됨
+
+    user_type = Column(Enum("Standard", "Business"))
+
+    is_additional_info_provided = Column(Boolean, default=False)
+
+    auth_provider = Column(Enum("Google", "Naver", "Kakao", "None"))
+    auth_provider_id = Column(String, nullable=True)  # OAuth 공급자의 고유 식별자
+
+    profile_picture_url = Column(String, nullable=True)  # 프로필 사진 URL
 
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    oauth_accounts = relationship("UserOAuth", back_populates="user")
-    business_info = relationship("BusinessUser", back_populates="user", uselist=False)
-
-
-class UserOAuth(Base):
-    __tablename__ = "user_oauth"
-
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
-    provider_name = Column(String)
-    provider_user_id = Column(String)
-    # access_token = Column(String)
-    # refresh_token = Column(String) <-- Oauth provider의 token임 authlib으로 얻어와야하는거
-
-    user = relationship("User", back_populates="oauth_accounts")
+    business_user = relationship("BusinessUser", back_populates="user", uselist=False)
 
 
 class BusinessUser(Base):
     __tablename__ = "business_users"
 
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"))
-    company_name = Column(String)
+    id = Column(Integer, ForeignKey("users.id"), primary_key=True)
+    company_info = Column(Text)
+    company_email = Column(String)
     company_address = Column(String)
-    contact_number = Column(String)
-    approval_status = Column(String, default="pending")
 
-    user = relationship("User", back_populates="business_info")
+    user = relationship("User", back_populates="business_user")
