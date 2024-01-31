a2b1114d746e4b7b863f9940ce7da777f2af05d4
김치욱 <car_ddong@naver.com>
2024-02-01 01:49:39 +0900

    feat: profile axios, button fix

--- front/src/components/ImageUpload.js
+++ front/src/components/ImageUpload.js
@@ -1,8 +1,4 @@
-import { useState } from "react";
-
-const ImageUpload = () => {
-  const [uploadImgUrl, setUploadImgUrl] = useState();
-
+const ImageUpload = ({ setImage, image }) => {
   const onChangeImageUpload = (e) => {
     const { files } = e.target;
     const uploadFile = files[0];
@@ -10,7 +6,7 @@ const ImageUpload = () => {
       const reader = new FileReader();
       reader.readAsDataURL(uploadFile);
       reader.onloadend = () => {
-        setUploadImgUrl(reader.result);
+        setImage(reader.result);
       };
     } else {
       console.error("잘못된 파일 타입. Blob을 기대했습니다.");
@@ -20,12 +16,12 @@ const ImageUpload = () => {
     <>
       <div className="flex-col w-[250px] h-full p-4 mr-4 border-4">
         <div className="grid place-items-center">
-          <img className="w-32 h-40" src={uploadImgUrl} alt="" />
+          <img className="w-32 h-40" src={image} alt="" />
         </div>
         <div className="grid place-content-end mt-2">
           <label
             className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
-            for="imgFile"
+            htmlFor="imgFile"
           >
             이미지 첨부
           </label>
