f3fecff72c093f9d4a4d4ff92ea3135210594fa2
김치욱 <car_ddong@naver.com>
2024-01-31 10:08:42 +0900

    feat: update tags

--- front/src/pages/profile/Profile.js
+++ front/src/pages/profile/Profile.js
@@ -1,16 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 import ImageUpload from "../../components/ImageUpload";
 import axios from "axios";
 
-const Profile = ({ email, name }) => {
-  const [changeEmail, setEmail] = useState(email);
-  const [changeName, setName] = useState(name);
-  const [] = useState();
-  const [changeInfo, setChangeInfo] = useState(false);
-  const changelick = () => {
-    setChangeInfo((prevChangeInfo) => !prevChangeInfo);
-  };
+const Profile = () => {
+  const navigate = useNavigate();
+
+  // 초기 값
+  const [Email, setEmail] = useState("");
+  const [name, setName] = useState("");
+
+  // 프로필 변경 버튼 클릭 시
+  const [changeEmail, setChangeEmail] = useState();
+  const [changeName, setChangeName] = useState();
+
+  // 프로필 변경 버튼 클릭 시 잠겨있는 블럭 해제
+  const [buttonClick, setButtonClick] = useState(false);
 
   const API_URL = "https://localhost:8000";
 
@@ -20,11 +26,19 @@ const Profile = ({ email, name }) => {
       .get(`${API_URL}/profile`)
       .then((response) => {
         console.log(response);
+        // 이 부분 값 확인 후 수정
+        setEmail(response.data);
+        setName(response.data);
       })
       .catch((error) => {
         console.error(error);
       });
-  });
+  }, []);
+
+  const emailHandler = (e) => {
+    const currentCode = e.target.value;
+    setEmail(currentCode);
+  };
 
   // 프로필 사진 첨부
   const profileUpdate = async (e) => {
@@ -38,6 +52,10 @@ const Profile = ({ email, name }) => {
     } catch (error) {
       console.error(error);
     }
+  };
+
+  const backButton = () => {
+    navigate("/");
   };
 
   return (
@@ -55,12 +73,23 @@ const Profile = ({ email, name }) => {
             <form className="flex flex-col p-2">
               <div className="flex flex-col">
                 <label htmlFor="email">이메일</label>
-                <input
-                  className="border-b-2 rounded m-2 bg-gray-100"
-                  disabled
-                  type="text"
-                  id="email"
-                />
+                {!buttonClick ? (
+                  <input
+                    className="border-b-2 rounded m-2 bg-gray-100"
+                    disabled
+                    type="text"
+                    id="email"
+                    value={Email}
+                  />
+                ) : (
+                  <input
+                    className="border-b-2 rounded m-2 bg-white"
+                    type="text"
+                    id="email"
+                    onChange={emailHandler}
+                    value={Email}
+                  />
+                )}
               </div>
               <div className="pt-4 flex flex-col">
                 <input
@@ -73,23 +102,43 @@ const Profile = ({ email, name }) => {
                 <label className="" htmlFor="name">
                   이름
                 </label>
-                <input
-                  className="border-b-2 rounded m-2 w-48"
-                  type="text"
-                  id="name"
-                  value={name}
-                />
+                {!buttonClick ? (
+                  <input
+                    className="border-b-2 rounded m-2 w-48"
+                    type="text"
+                    id="name"
+                    value={name}
+                    disabled
+                  />
+                ) : (
+                  <input
+                    className="border-b-2 rounded m-2 w-48"
+                    type="text"
+                    id="name"
+                    value={name}
+                  />
+                )}
               </div>
               <div className="flex justify-between mt-8">
-                <button className="bg-gray-500 text-white py-2 px-8 rounded cursor-pointer">
+                <button
+                  className="bg-gray-500 text-white py-2 px-8 rounded cursor-pointer"
+                  onClick={backButton}
+                >
                   뒤로가기
                 </button>
-                <input
-                  className="bg-blue-500 text-white py-2 px-8 rounded cursor-pointer mt-2"
-                  type="submit"
-                  //   value={changeIn0fo ? "수정하기" : "수정완료"}
-                  //   onClick={changeClick}
-                />
+                {!buttonClick ? (
+                  <input
+                    className="bg-blue-500 text-white py-2 px-8 rounded cursor-pointer mt-2"
+                    type="button"
+                    value="수정"
+                  />
+                ) : (
+                  <input
+                    className="bg-blue-500 text-white py-2 px-8 rounded cursor-pointer mt-2"
+                    type="submit"
+                    value="제출하기"
+                  />
+                )}
               </div>
             </form>
           </div>
