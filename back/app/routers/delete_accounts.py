9b7eefca58bfa3748cdfecfbf3d06cd07e368838
Chosamee <cytor@naver.com>
2024-01-31 16:40:56 +0900

    fix: Fix run app

--- back/app/routers/delete_accounts.py
+++ back/app/routers/delete_accounts.py
@@ -2,16 +2,16 @@ import os
 from dotenv import load_dotenv
 from fastapi import APIRouter, Depends, Form, HTTPException, Request, Response
 from sqlalchemy.orm import Session
-from app.database import get_db
-from app.models.user import User
-from app.services.auth_service import hash_password, verify_password
-from app.schemas.user_schema import UserLogin
-from ..routers import oauth_login, room_handler
-from app.schemas.user_schema import UserCreate
-from app.schemas.user_schema import UserCreate, UserLogin
-from ..database import get_db
-from app.models.user import BusinessUser, EmailVerification, User
-from app.services.auth_service import (
+from database import get_db
+from models.user import User
+from services.auth_service import get_user_by_token, hash_password, verify_password
+from schemas.user_schema import UserLogin
+from routers import oauth_login, room_handler
+from schemas.user_schema import UserCreate
+from schemas.user_schema import UserCreate, UserLogin
+from database import get_db
+from models.user import BusinessUser, EmailVerification, User
+from services.auth_service import (
     create_access_token,
     generate_internal_id,
     hash_password,
@@ -35,11 +35,7 @@ async def delete_accounts(
     password: str = Form(None),
     db: Session = Depends(get_db),
 ):
-    token = request.get.cookie("__Host-access_token")
-    payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
-    internal_id = payload.get("sub")
-
-    user = db.query(User).filter(User.internal_id == internal_id).first()
+    user = get_user_by_token(request, db)
     if not user:
         raise HTTPException(status_code=404, detail="Not found User")
     if email != user.email:
--- back/app/services/auth_service.py
+++ back/app/services/auth_service.py
@@ -2,9 +2,15 @@ import datetime
 import hashlib
 import os
 import secrets
-from jose import jwt
-import bcrypt
 import uuid
+import bcrypt
+from dotenv import load_dotenv
+from fastapi import HTTPException, status
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+
+from models.user import User
+
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 ALGORITHM = "HS256"
@@ -22,7 +28,7 @@ def generate_random_state(length=32):
 
 # 엑세스 토큰 생성
 def create_access_token(internal_id: str):
-    expire = datetime.datetime.utcnow() + datetime.timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode = {"sub": internal_id, "exp": expire}
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
@@ -57,3 +63,38 @@ def verify_password(password, hashed):
 
 def generate_internal_id():
     return str(uuid.uuid4())
+
+
+# token으로부터 internal_id 추출
+def verify_token(token: str):
+    if token is None:
+        raise HTTPException(status_code=400, detail="이재민 바보")
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        internal_id = payload.get("sub")
+        if internal_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not validate credentials",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return internal_id
+
+
+# token으로부터 user객체 획득
+def get_user_by_token(request, db):
+    token = request.cookies.get("__Host-access_token")
+    if not token or not verify_token(token):
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+    internal_id = verify_token(token)
+    user = db.query(User).filter(User.internal_id == internal_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="Not found User")
+    return user
