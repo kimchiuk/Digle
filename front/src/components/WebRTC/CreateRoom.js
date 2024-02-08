8a3135138ee375c309eebb6d6a844d50097c1d72
Anhyunsung <gustjd980112@yu.ac.kr>
2024-02-08 14:53:37 +0900

    방 목록이랑 방생성 합침

--- front/src/components/WebRTC/CreateRoom.js
+++ front/src/components/WebRTC/CreateRoom.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import RoomList from "./admin";
+import reload from "../../assets/webRTC/refresh.png";
 
 import MainImg from "../../assets/main.png";
 
 function CreateRoom() {
   const [room_id, setRoomId] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+  const [refresh, setRefresh] = useState(false);
   const navigate = useNavigate();
   const API_URL = process.env.REACT_APP_API_BASE_URL;
+
   const handleJoinRoom = async () => {
     setIsLoading(true);
     try {
@@ -21,8 +25,15 @@ function CreateRoom() {
           console.log("room_id : ", response.data.plugindata.data.room);
           const newRoomId = response.data.plugindata.data.room;
           setRoomId(newRoomId);
-          alert("방이 생성되었습니다");
-          navigate(`/anhs?roomId=${newRoomId}&userId=user123&role=publisher`);
+
+          const confirm = window.confirm(
+            "방이 생성되었습니다. 바로 입장하시겠습니까?"
+          );
+          if (confirm) {
+            navigate(`/anhs?roomId=${newRoomId}&userId=user123&role=publisher`);
+          } else {
+            setRefresh(!refresh);
+          }
         })
         .catch((err) => {
           console.log(err);
@@ -33,19 +44,32 @@ function CreateRoom() {
     setIsLoading(false);
   };
 
+  const handleRefresh = () => {
+    setRefresh(!refresh); // refresh 상태를 변경
+  };
+
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-200">
+    <div className="flex items-center justify-center min-h-screen bg-gray-200 ">
       <img className="absolute w-full h-full object-cover" src={MainImg} />
-      <div className="relative p-8 bg-black bg-opacity-70 rounded shadow-md max-w-sm w-full">
-        <div className="flex flex-col space-y-6">
-          <button
-            className="text-white"
-            onClick={handleJoinRoom}
-            disabled={isLoading}
-          >
-            {isLoading ? "로딩 중" : "방 만들고 입장"}
+      <div className="relative p-8 overflow-y-scroll overflow-y-hidden bg-white bg-opacity-70 rounded-2xl shadow-md max-w-sm w-full max-h-[400px] min-h-[300px] ">
+        <div className="flex pb-5">
+          <h1 className="font-bold">회의실 목록</h1>
+          <button className="" onClick={handleRefresh}>
+            <img className="w-4 h-4 ml-2" src={reload} alt="새로고침" />
           </button>
+          <div className="flex items-end justify-end flex-grow">
+        <button
+          className="font-bold"
+          onClick={handleJoinRoom}
+          disabled={isLoading}
+        >
+          {isLoading ? "로딩 중" : "방 생성하기"}
+        </button>
+      </div>
         </div>
+        <div className="flex flex-col space-y-6">
+        </div>
+        <RoomList refresh={refresh} />
       </div>
     </div>
   );
--- front/src/components/WebRTC/admin.js
+++ front/src/components/WebRTC/admin.js
@@ -1,93 +1,104 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState, useCallback } from "react";
+import axios from "axios";
 
-function RoomList() {
-    const [rooms, setRooms] = useState([]);
-    const [participants, setParticipants] = useState({});
-    const [showParticipants, setShowParticipants] = useState({}); // 각 방의 참여자 목록 표시 상태를 관리하는 상태
+function RoomList({ refresh }) {
+  const [rooms, setRooms] = useState([]);
+  const [participants, setParticipants] = useState({});
+  const [showParticipants, setShowParticipants] = useState({}); // 각 방의 참여자 목록 표시 상태를 관리하는 상태
 
-    useEffect(() => {
-        axios.get('https://localhost:8000/rooms/list')
-            .then(response => {
-                const fetchedRooms = response.data.plugindata.data.list;
-                setRooms(fetchedRooms);
-                // 초기화 시 모든 방에 대해 참여자 목록을 숨김 상태로 설정
-                let initialShowState = {};
-                fetchedRooms.forEach(room => {
-                    initialShowState[room.room] = false;
-                });
-                setShowParticipants(initialShowState);
-            })
-            .catch(error => {
-                console.error("Error fetching rooms: ", error);
-            });
-    }, []);
+  const API_URL = process.env.REACT_APP_API_BASE_URL;
 
-    const handleDeleteRoom = (room_id) => {
-        axios.post(`https://localhost:8000/rooms/${room_id}/destroy`)
-            .then(() => {
-                const updatedRooms = rooms.filter(room => room.room !== room_id);
-                setRooms(updatedRooms);
-            })
-            .catch(error => {
-                console.error("Error deleting room: ", error);
-            });
-    };
+  const fetchRooms = useCallback(() => {
+    axios
+      .get(`${API_URL}/rooms/list`)
+      .then((response) => {
+        const fetchedRooms = response.data.plugindata.data.list;
+        setRooms(fetchedRooms);
 
-    const handleShowParticipants = async (room_id) => {
-        // 참여자 목록 표시 상태를 토글
-        setShowParticipants(prevState => ({
-            ...prevState,
-            [room_id]: !prevState[room_id]
-        }));
-    
-        // 이미 참여자 목록을 가져온 상태라면 함수를 빠져나간다.
-        if (participants[room_id]) return;
-    
-        try {
-            const response = await axios.get(`https://localhost:8000/rooms/${room_id}/participants`);
-            console.log(response);
-            const fetchedParticipants = response.data;
-            
-            setParticipants(prevParticipants => ({
-                ...prevParticipants,
-                [room_id]: fetchedParticipants.length > 0 ? fetchedParticipants : "NoParticipants"
-            }));
-        } catch (error) {
-            console.error("Error fetching participants: ", error);
-        }
-    };
-    
+        // 초기화 시 모든 방에 대해 참여자 목록을 숨김 상태로 설정
+        let initialShowState = {};
+        fetchedRooms.forEach((room) => {
+          initialShowState[room.room] = false;
+        });
+        setShowParticipants(initialShowState);
+      })
+      .catch((error) => {
+        console.error("Error fetching rooms: ", error);
+      });
+  }, [API_URL]); // API_URL이 변경될 때만 함수가 새로 생성되도록 의존성 배열에 추가
 
-    return (
-        <div>
-            <h2>Room List</h2>
-            <ul>
+  useEffect(() => {
+    fetchRooms(); // 방 목록을 가져오는 함수를 호출
+  }, [refresh, fetchRooms]); // refresh, fetchRooms가 변경될 때 effect가 실행되도록 의존성 배열에 추가
 
-                {rooms.map(room => (
-    <li key={room.room}>
-        {room.description}
-        <button onClick={() => handleDeleteRoom(room.room)}>Delete</button>
-        <button onClick={() => handleShowParticipants(room.room)}>
-            {showParticipants[room.room] ? '참여자 숨기기' : '참여자 목록'}
-        </button>
-        {showParticipants[room.room] && (
-            Array.isArray(participants[room.room]) ? (
+  const handleDeleteRoom = (room_id) => {
+    axios
+      .post(`${API_URL}/rooms/${room_id}/destroy`)
+      .then(() => {
+        const updatedRooms = rooms.filter((room) => room.room !== room_id);
+        setRooms(updatedRooms);
+      })
+      .catch((error) => {
+        console.error("Error deleting room: ", error);
+      });
+  };
+
+  const handleShowParticipants = async (room_id) => {
+    // 참여자 목록 표시 상태를 토글
+    setShowParticipants((prevState) => ({
+      ...prevState,
+      [room_id]: !prevState[room_id],
+    }));
+
+    // 이미 참여자 목록을 가져온 상태라면 함수를 빠져나간다.
+    if (participants[room_id]) return;
+
+    try {
+      const response = await axios.get(
+        `${API_URL}/rooms/${room_id}/participants`
+      );
+      console.log(response);
+      const fetchedParticipants = response.data;
+
+      setParticipants((prevParticipants) => ({
+        ...prevParticipants,
+        [room_id]:
+          fetchedParticipants.length > 0
+            ? fetchedParticipants
+            : "NoParticipants",
+      }));
+    } catch (error) {
+      console.error("Error fetching participants: ", error);
+    }
+  };
+
+  return (
+    <div className="">
+      <ul>
+        {rooms.map((room) => (
+          <li 
+          className="bg-sky-300 text-white rounded-2xl p-3 mb-3"
+          key={room.room}>
+            {room.description}
+            <button onClick={() => handleDeleteRoom(room.room)}>Delete</button>
+            <button onClick={() => handleShowParticipants(room.room)}>
+              {showParticipants[room.room] ? "참여자 숨기기" : "참여자 목록"}
+            </button>
+            {showParticipants[room.room] &&
+              (Array.isArray(participants[room.room]) ? (
                 <ul>
-                    {participants[room.room].map(participant => (
-                        <li key={participant.id}>{participant.display}</li>
-                    ))}
+                  {participants[room.room].map((participant) => (
+                    <li key={participant.id}>{participant.display}</li>
+                  ))}
                 </ul>
-            ) : (
+              ) : (
                 <p>방에 아무도 존재하지 않습니다.</p>
-            )
-        )}
-    </li>
-))}
-
-            </ul>
-        </div>
-    );
+              ))}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default RoomList;
