413c9eb4645dc8bcc7e3d14605979797b33d6c3f
bogeun <kimbo3212@gmail.com>
2024-02-13 16:50:44 +0900

    죽여줘

--- back/app/routers/oauth_login.py
+++ back/app/routers/oauth_login.py
@@ -208,59 +208,69 @@ async def login_for_access_token(
         user_info = user_info_data["kakao_account"]
         print(user_info)
 
-        # db에 해당 유저가 있는지 확인
-        user = db.query(User).filter(User.email == user_info["email"]).first()
-
-        # 만약 해당 유저가 없음. -> 회원가입
-        if user is None:
-            # 객체로 생성
-            user = User(
-                email=user_info["email"],
-                name=user_info["profile"]["nickname"],
-                internal_id=generate_internal_id(),
-                is_additional_info_provided=False,
-                auth_provider="Kakao",
-                user_type="Standard",
-            )
-            db.add(user)
-            db.commit()
-            db.refresh(user)
-
-            # response 생성 status 200
-            return_value = JSONResponse(
-                status_code=200,
-                content={"message": "New User", "action": "request_additional_info"},
-                headers=dict(response.headers),
-            )
-        elif not user.is_additional_info_provided:
-            # 추가 정보가 아직 제출되지 않은 경우
-            return_value = JSONResponse(
-                status_code=200,
-                content={
-                    "message": "Additional Info Required",
-                    "action": "request_additional_info",
-                },
-                headers=dict(response.headers),
-            )
-        else:
-            return_value = JSONResponse(
-                status_code=200,
-                content={"message": "login complete", "user": user.email},
-                headers=dict(response.headers),
-            )
-
-            # 그걸로 access token을 생성
-        access_token = create_access_token(user.internal_id, "service_access")
-        # access token을 보안때문에 header에다 cookie를 담아서 줄것.
-        response.set_cookie(
-            # key="access_token",
-            key="__Host-access_token",
-            value=access_token,
-            httponly=True,
-            secure=True,
-            samesite="None",
-            # domain = '어쩌고 저쩌고',
-            path="/",  # 전체 경로에서 사용
-            max_age=3600,  # 예: 1시간 유효기간
+        message, action, response = user_db_login(
+            "Kakao", user_info_data["kakao_account"], user_info_data, db, response
         )
-    return return_value
+
+    return JSONResponse(
+        status_code=200,
+        content={"message": message, "action": action},
+        headers=dict(response.headers),
+    )
+
+    #     # db에 해당 유저가 있는지 확인
+    #     user = db.query(User).filter(User.email == user_info["email"]).first()
+
+    #     # 만약 해당 유저가 없음. -> 회원가입
+    #     if user is None:
+    #         # 객체로 생성
+    #         user = User(
+    #             email=user_info["email"],
+    #             name=user_info["profile"]["nickname"],
+    #             internal_id=generate_internal_id(),
+    #             is_additional_info_provided=False,
+    #             auth_provider="Kakao",
+    #             user_type="Standard",
+    #         )
+    #         db.add(user)
+    #         db.commit()
+    #         db.refresh(user)
+
+    #         # response 생성 status 200
+    #         return_value = JSONResponse(
+    #             status_code=200,
+    #             content={"message": "New User", "action": "request_additional_info"},
+    #             headers=dict(response.headers),
+    #         )
+    #     elif not user.is_additional_info_provided:
+    #         # 추가 정보가 아직 제출되지 않은 경우
+    #         return_value = JSONResponse(
+    #             status_code=200,
+    #             content={
+    #                 "message": "Additional Info Required",
+    #                 "action": "request_additional_info",
+    #             },
+    #             headers=dict(response.headers),
+    #         )
+    #     else:
+    #         return_value = JSONResponse(
+    #             status_code=200,
+    #             content={"message": "login complete", "user": user.email},
+    #             headers=dict(response.headers),
+    #         )
+
+    #         # 그걸로 access token을 생성
+    #     access_token = create_access_token(user.internal_id, "service_access")
+    #     # access token을 보안때문에 header에다 cookie를 담아서 줄것.
+    #     response.set_cookie(
+    #         # key="access_token",
+    #         key="__Host-access_token",
+    #         value=access_token,
+    #         httponly=True,
+    #         secure=True,
+    #         samesite="None",
+    #         # domain = '어쩌고 저쩌고',
+    #         path="/",  # 전체 경로에서 사용
+    #         max_age=3600,  # 예: 1시간 유효기간
+    #     )
+    # return return_value
